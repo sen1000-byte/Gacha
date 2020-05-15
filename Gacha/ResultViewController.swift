39ba4a0e37c82705e83da436d16c83b9fa134459
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-15 16:46:25 +0900

    レアとかの言葉を入れたい

---
+++ Gacha/ResultViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultViewController.swift
+//  Gacha
+//
+//  Created by Chihiro Nishiwaki on 2020/05/15.
+//  Copyright © 2020 nishiwaki sen. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var number: Int!
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var monsterImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        number = Int.random(in:0...9)
+        if number == 9{
+            monsterImageView.image = UIImage(named: "monster010")
+            backgroundImageView.image = UIImage(named: "bg_gold")
+        }else if number > 6 {
+            monsterImageView.image = UIImage(named: "monster006")
+            backgroundImageView.image = UIImage(named: "bg_red")
+        }else{
+            monsterImageView.image = UIImage(named: "monster003")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+        }
+    }
+    
+    @IBAction func back(){
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
